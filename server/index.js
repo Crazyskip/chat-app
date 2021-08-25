5c1807d4f24b838ff696f7f8744a352ddacfd571
Damon Jensen <crazyskip73@gmail.com>
2021-08-25 12:43:52 +1000

    initial frontend and server commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var http = require("http").Server(app);
+var path = require("path");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "../dist/chat-app/")));
+
+let server = http.listen(3000, function () {
+  let port = server.address().port;
+  console.log(`Server listening on port: ${port}`);
+});
